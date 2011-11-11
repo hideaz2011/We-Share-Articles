c85cbf777921f15b35a59e9f0d557b6c0edf0858
Rajavel Balasubramanian <hideaz2011@gmail.com>
2011-11-11 14:00:50 +0000

    change default.php

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -34,37 +34,53 @@ endif; ?>
 
 	<div id="toppanel">
 		<div id="panel">
-			<div class="content clearfix">
-			<div class="left">
-				<h1>The We Share Articles</h1>
-				<h2>A register/login solution</h2>		
-				<p class="grey">You are free to use this login and registration system in you sites!</p>
-            </div>    
-<form action="<?php echo JRoute::_( 'index.php', true, $params->get('usesecure')); ?>" method="post" name="login" id="form-login" >
+        	<!--div id="panelL">
+        	<div id="panelM">
+            	<div id="panelR"-->
+        	<div id="panelBR">
+            	<div id="panelBL">
+               		<div id="panelTR">
+                    	<div id="panelTL">
+                        
+                     <div class="content clearfix">
+			   <div id="dhtmlgoodies_tabView1">
+					<div class="dhtmlgoodies_aTab">
+		        <form action="<?php echo JRoute::_( 'index.php', true, $params->get('usesecure')); ?>" method="post" name="login" id="form-login" >
 	<?php echo $params->get('pretext'); ?>
 	<fieldset class="input">
-	<p id="form-login-username">
-		<label for="modlgn_username"><?php echo JText::_('Username') ?></label><br />
-		<input id="modlgn_username" type="text" name="username" class="inputbox" alt="username" size="18" />
+    <p id="form-login-lostpassword">
+    	<a href="<?php echo JRoute::_( 'index.php?option=com_user&view=reset' ); ?>">
+		<?php echo JText::_('Lost Your password?'); ?></a>
+	</p>
+
+    <p id="form-login-username">
+		<input id="modlgn_username" type="text" name="username" class="inputbox" alt="username" placeholder="Username" size="17" />
 	</p>
 	<p id="form-login-password">
-		<label for="modlgn_passwd"><?php echo JText::_('Password') ?></label><br />
-		<input id="modlgn_passwd" type="password" name="passwd" class="inputbox" size="18" alt="password" />
+			<input id="modlgn_passwd" type="password" name="passwd" class="inputbox"  placeholder="Password" size="17" alt="password" />
 	</p>
+    <!--p id="submit">Go</p-->
+	<input type="submit" name="Submit" id ="submit" class="button" value="<?php echo JText::_('GO') ?>" />
+	
 	<?php if(JPluginHelper::isEnabled('system', 'remember')) : ?>
 	<p id="form-login-remember">
-		<label for="modlgn_remember"><?php echo JText::_('Remember me') ?></label>
-		<input id="modlgn_remember" type="checkbox" name="remember" class="inputbox" value="yes" alt="Remember Me" />
-	</p>
+		<input id="modlgn_remember" type="checkbox" name="remember" class="inputbox" value="yes" alt="Remember Me" /> 
+        <label id="modlgn_remember"><?php echo JText::_('Remember me') ?></label>
+ 	</p>
+	
 	<?php endif; ?>
-	<input type="submit" name="Submit" class="button" value="<?php echo JText::_('LOGIN') ?>" />
 	</fieldset>
-	<ul>
-		<li>
-			<a href="<?php echo JRoute::_( 'index.php?option=com_user&view=reset' ); ?>">
-			<?php echo JText::_('FORGOT_YOUR_PASSWORD'); ?></a>
-		</li>
-		<li>
+	<div id="loginlogo">
+    	<p id="orlogin">or</p>
+    	<p id="loginimg">&nbsp;</p>
+        
+    </div>
+    <div id="facebookimg">
+    <p id="faceimg">&nbsp;</p>
+    <p id="facetext"><a href="#">Login with facebook</a></p>
+    </div>
+    <!--ul>
+				<li>
 			<a href="<?php echo JRoute::_( 'index.php?option=com_user&view=remind' ); ?>">
 			<?php echo JText::_('FORGOT_YOUR_USERNAME'); ?></a>
 		</li>
@@ -76,7 +92,7 @@ endif; ?>
 				<?php echo JText::_('REGISTER'); ?></a>
 		</li>
 		<?php endif; ?>
-	</ul>
+	</ul-->
 	<?php echo $params->get('posttext'); ?>
 
 	<input type="hidden" name="option" value="com_user" />
@@ -84,22 +100,54 @@ endif; ?>
 	<input type="hidden" name="return" value="<?php echo $return; ?>" />
 	<?php echo JHTML::_( 'form.token' ); ?>
 </form>
-</div>
-</div>
-	<div class="tab">
+	</div>
+	<div class="dhtmlgoodies_aTab">
+		<div id="Registerform">
+        <ul>
+        <li><p>
+		<input id="modlgn_username" type="text" name="emailid" class="inputbox" alt="username" placeholder="E-mail id" size="28" /></p></li>
+		
+	<li><p>
+			<input id="modlgn_passwd" type="password" name="passwd" class="inputbox"  placeholder="Password" size="17" alt="password" />
+	</p></li>
+    <li><p>
+			<input id="modlgn_passwd" type="confirmpassword" name="confirmpasswd" class="inputbox"  placeholder="Confirm Password" size="17" alt="password" />
+	</p></li>
+    </ul>
+    <p id="regimg">&nbsp;</p>
+	</div>
+    <div id="Register">
+     <p id="regstext"><a href="#">Register Now</a></p>
+         <p id="regsimg">&nbsp;</p>
+    </div>
+	</div>
+    </div>
+    </div>
+<script type="text/javascript">
+initTabs('dhtmlgoodies_tabView1',Array('Login','Register'),0,426,188,Array(true,true));
+</script>
+  
+  
+     </div>
+     </div>
+     </div>  
+      </div>
+      </div>
+               
+
+    <div class="tab">
 		<ul class="login">
 	    	<li class="left">&nbsp;</li>
-	        <li>Hello <?php echo $_SESSION['usr'] ? $_SESSION['usr'] : 'Guest';?>!</li>
-			<li class="sep">|</li>
-			<li id="toggle">
-				<a id="open" class="open" href="#"><?php echo $_SESSION['id']?'Open Panel':'Log In | Register';?></a>
-				<a id="close" style="display: none;" class="close" href="#">Close Panel</a>			
+	        <li id="toggle">
+				<a id="open" class="open" href="#"><?php echo $_SESSION['id']?'Open Panel':'Log In';?></a>
+				<a id="close" style="display: none;" class="close" href="#">Close</a>			
 			</li>
 	    	<li class="right">&nbsp;</li>
 		</ul> 
 	</div> <!-- / top -->
 	
-
 </div>
+
+
 
 <?php endif; ?>

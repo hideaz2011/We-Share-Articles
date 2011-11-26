2847d8dd0e830e27cbc6fbbc7445a290c54b4bf5
Narendran Jeyaraman <naren.hideaz@gmail.com>
2011-11-26 18:35:47 +0530

    section Maries

--- configuration.php
+++ configuration.php
@@ -33,7 +33,7 @@ class JConfig {
 	var $dbtype = 'mysql';
 	var $host = 'localhost';
 	var $user = 'root';
-	var $db = 'wesharearticles';
+	var $db = 'git_weshare_git';
 	var $dbprefix = 'jos_';
 	var $mailer = 'mail';
 	var $mailfrom = 'admin@wesharearticles.com';
